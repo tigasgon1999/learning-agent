Achieved 20/20 Fixed learn function silent bug

ruagomesfreiregame2sol.py
@@ -18,14 +18,21 @@ class LearningAgent:
         # define this function
         self.nS = nS
         self.nA = nA
+
         # initialize Q-table
-        self.Q = np.zeros((nS, nA))
+        self.Q = np.zeros((nS, nA), dtype=float)
+        self.statePlayNum = [-1] * nS
         self.epsilon = 0.2
-        self.alpha = 0.5
-        self.gamma = 0.9
+        self.alpha = 0.6
+        self.gamma = 0.8
         self.moveCounter = 0
         self.lastState = -1
+        self.exploit = 0
+        self.explore = 0
         # self.vis = Visualizer(600, 1000, 10, 4)
+
+    def getQ(self):
+        return self.Q
 
     # Select one action, used when learning
     # st - is the current state
@@ -33,22 +40,28 @@ class LearningAgent:
     # for a given state they are always given in the same order
     # returns
     # a - the index to the action in aa
+
     def selectactiontolearn(self, st, aa):
         # define this function
         # print("select one action to learn better")
         # self.moveCounter += 1
+        # self.moveCounter += 1
         # if not (self.moveCounter % 15):
-        #     self.epsilon *= 0.9
+        #     self.epsilon *= 1.2
+
+        if self.statePlayNum[st] is -1:
+            self.statePlayNum[st] = len(aa)
+
         a = -1
         if random.uniform(0, 1) < self.epsilon:
             a = random.randrange(0, len(aa))
-            while aa[a] is self.lastState:
-                a = random.randrange(0, len(aa))
+            self.explore += 1
+            # while aa[a] is self.lastState:
+            #     a = random.randrange(0, len(aa))
         else:
-            # TODO: change this to numpy.max
             a = np.argmax(self.Q[st][:len(aa)])
-            while aa[a] is self.lastState:
-                a = np.argmax(np.delete(self.Q[st][:len(aa)], a))        
+            self.exploit += 1
+        
         return a
 
     # Select one action, used when evaluating
@@ -58,10 +71,8 @@ class LearningAgent:
     # returns
     # a - the index to the action in aa
     def selectactiontoexecute(self, st, aa):
-        vis.saveTable(self.Q)
-        # self.printQ()
-        self.moveCounter = 0
-        self.epsilon = 1
+        # vis.saveTable(self.Q)
+        # self.epsilon = 1
         # define this function
         a = np.argmax(self.Q[st][:len(aa)])
         # print("select one action to see if I learned")
@@ -76,7 +87,7 @@ class LearningAgent:
         # define this function
         # print("learn something from this data")
         bestNextQ = -math.inf
-        for q in self.Q[nst]:
+        for q in self.Q[nst][:self.statePlayNum[nst]]:
             if q > bestNextQ:
                 bestNextQ = q
 