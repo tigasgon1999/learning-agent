46cfdc0e68c083636a1fcde7e0b5b1b6413651b4
João Porto <joaofporto@tecnico.ulisboa.pt>
2019-12-05 00:37:48 +0000

    Cleaned code

--- ruagomesfreiregame2sol.py
+++ ruagomesfreiregame2sol.py
@@ -11,11 +11,9 @@ import visualizer as vis
 
 class LearningAgent:
 
-    # init
     # nS maximum number of states
     # nA maximum number of action per state
     def __init__(self, nS, nA):
-        # define this function
         self.nS = nS
         self.nA = nA
 
@@ -25,14 +23,6 @@ class LearningAgent:
         self.epsilon = 0.2
         self.alpha = 0.6
         self.gamma = 0.8
-        self.moveCounter = 0
-        self.lastState = -1
-        self.exploit = 0
-        self.explore = 0
-        # self.vis = Visualizer(600, 1000, 10, 4)
-
-    def getQ(self):
-        return self.Q
 
     # Select one action, used when learning
     # st - is the current state
@@ -55,13 +45,8 @@ class LearningAgent:
         a = -1
         if random.uniform(0, 1) < self.epsilon:
             a = random.randrange(0, len(aa))
-            self.explore += 1
-            # while aa[a] is self.lastState:
-            #     a = random.randrange(0, len(aa))
         else:
             a = np.argmax(self.Q[st][:len(aa)])
-            self.exploit += 1
-        
         return a
 
     # Select one action, used when evaluating
@@ -71,11 +56,8 @@ class LearningAgent:
     # returns
     # a - the index to the action in aa
     def selectactiontoexecute(self, st, aa):
-        # vis.saveTable(self.Q)
-        # self.epsilon = 1
-        # define this function
+        # Select action with max q value
         a = np.argmax(self.Q[st][:len(aa)])
-        # print("select one action to see if I learned")
         return a
 
     # this function is called after every action
@@ -84,22 +66,13 @@ class LearningAgent:
     # a - the index to the action taken
     # r - reward obtained
     def learn(self, ost, nst, a, r):
-        # define this function
-        # print("learn something from this data")
+        # Caclulate q value for state-action pair
         bestNextQ = -math.inf
         for q in self.Q[nst][:self.statePlayNum[nst]]:
             if q > bestNextQ:
                 bestNextQ = q
 
-        qTarget = r + self.gamma*bestNextQ
+        qTarget = r + self.gamma * bestNextQ
         qDelta = qTarget - self.Q[ost][a]
-        self.Q[ost][a] += self.alpha*qDelta
-        # self.vis.update(ost, a, self.Q[ost][a])
-
+        self.Q[ost][a] += self.alpha * qDelta
         return
-
-    def printQ(self):
-        i = 0
-        for st in self.Q:
-            print(i, ":", st)
-            i += 1
